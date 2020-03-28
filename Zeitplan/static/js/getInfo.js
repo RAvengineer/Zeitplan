7d978e1e97294a17cad639d04b3e9781c9628c81
Rahul Bera <bera2591@gmail.com>
2020-03-28 22:52:46 +0530

    getInfo.js: Add main function

--- Zeitplan/static/js/getInfo.js
+++ Zeitplan/static/js/getInfo.js
@@ -9,17 +9,22 @@ $('#start_date').datepicker({
 // Event Colors available in Google Calendars
 var eventColors = ['#a4bdfc','#7ae7bf','#dbadff','#ff887c','#fbd75b','#ffb878','#46d6db','#e1e1e1','#5484ed','#51b749','#dc2127',]
 
-// Add background colors for <td> color circles
-for (let i = 1; i < 12; i++) {
-    var cid = "#c"+i;
-    $(cid).css("background-color",eventColors[i-1]);
+function main(){
+    // Add background colors for <td> color circles
+    for (let i = 1; i < 12; i++) {
+        var cid = "#c"+i;
+        $(cid).css("background-color",eventColors[i-1]);
+    }
+
+    // Add click listeners for <td> color circles
+    for (let i = 1; i < 12; i++) {
+        var cid = "#c"+i;
+        $(cid).on("click",function(){
+            $('#eventColorDummy').css("background-color",eventColors[i-1]);
+            $("#eventColor").attr("value",i);
+        });
+    }
+
 }
 
-// Add click listeners for <td> color circles
-for (let i = 1; i < 12; i++) {
-    var cid = "#c"+i;
-    $(cid).on("click",function(){
-        $('#eventColorDummy').css("background-color",eventColors[i-1]);
-        $("#eventColor").attr("value",i);
-    });
-}
+main();
