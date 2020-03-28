0fdd36ac0491a272af99e630d2c4f9372594d110
Rahul Bera <bera2591@gmail.com>
2020-03-28 22:15:28 +0530

    getInfo.html: Add comments

--- Zeitplan/static/js/getInfo.js
+++ Zeitplan/static/js/getInfo.js
@@ -1,3 +1,4 @@
+// Datepicker's script
 $('#end_date').datepicker({
     uiLibrary: 'bootstrap4'
 });
@@ -5,12 +6,16 @@ $('#start_date').datepicker({
     uiLibrary: 'bootstrap4'
 });
 
+// Event Colors available in Google Calendars
 var eventColors = ['#a4bdfc','#7ae7bf','#dbadff','#ff887c','#fbd75b','#ffb878','#46d6db','#e1e1e1','#5484ed','#51b749','#dc2127',]
+
+// Add background colors for <td> color circles
 for (let i = 1; i < 12; i++) {
     var cid = "#c"+i;
     $(cid).css("background-color",eventColors[i-1]);
 }
 
+// Add click listeners for <td> color circles
 for (let i = 1; i < 12; i++) {
     var cid = "#c"+i;
     $(cid).on("click",function(){
