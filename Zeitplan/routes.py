10433343c4d8297c82f632c9b21da402f575122e
Rahul Bera <bera2591@gmail.com>
2020-03-29 09:52:01 +0530

    routes.py: Use ParseTimeTable class

--- Zeitplan/routes.py
+++ Zeitplan/routes.py
@@ -1,11 +1,12 @@
 from flask import render_template, request, Response,jsonify
 from Zeitplan import app
+from Utilities.ParseTimeTable import ParseTimeTable
 
 # Variables
 
 
 # Objects/ Instances
-
+ptt = ParseTimeTable()
 
 # Routes for Templates
 @app.route('/')
@@ -16,10 +17,27 @@ def zeitplan():
 @app.route('/getData', methods=['GET', 'POST'])
 def getData():
     if request.method=='POST':
-        print(request.form['ttInText'])
+        ttInText = request.form.get('ttInText',' ')
+        start_date = request.form.get('start_date',' ')
+        if(request.form.get('recurringEvent',False,type=bool)):
+            end_date = request.form.get('end_date',-1)
+        calendarId = request.form.get('calendarId',"primary")
+        if(calendarId==''):
+            calendarId = "primary"
+        lst = ptt.convertTTtoEvents(ttInText,start_date)
+        # print(lst) # Debugging
+        print(lst)
+        if(lst==-1):
+            return render_template('sww.html')
         return render_template('getInfo.html')
     else:
         return render_template('error.html')
 
 
 # Routes for Responsive WebPages
+
+
+'''
+References:
+https://tedboy.github.io/flask/generated/generated/werkzeug.ImmutableMultiDict.get.html#werkzeug.ImmutableMultiDict.get
+'''
