00bd9a854803150f72453e111ad13d659b0b0ac8
Rahul Bera <bera2591@gmail.com>
2020-03-31 23:07:10 +0530

    CalendarAPI.py: Add getAuthorizationUrl()

--- Utilities/CalendarAPI.py
+++ Utilities/CalendarAPI.py
@@ -1,7 +1,17 @@
 # Imports
 from apiclient.discovery import build
-from google_auth_oauthlib.flow import InstalledAppFlow, json
+from google_auth_oauthlib.flow import InstalledAppFlow, json, Flow
 from google.oauth2.credentials import Credentials
+
+# This variable specifies the name of a file that contains the OAuth 2.0
+# information for this application, including its client_id and client_secret.
+CLIENT_SECRETS_FILE = "/home/RAvengineer/Zeitplan/Utilities/client_secret.json"
+
+# This OAuth 2.0 access scope allows for full read/write access to the
+# authenticated user's account and requires requests to use an SSL connection.
+SCOPES = ['https://www.googleapis.com/auth/calendar.events']
+API_SERVICE_NAME = 'calendar'
+API_VERSION = 'v3'
 
 class googleCalendarAPI():
     def __init__(self):
@@ -9,6 +19,24 @@ class googleCalendarAPI():
         Constructor for googleCalendarAPI
         '''
         self.service = None
+    
+    def getAuthorizationUrl(self,ruri):
+        # Create flow instance to manage the OAuth 2.0 Authorization Grant Flow steps.
+        flow = Flow.from_client_secrets_file(CLIENT_SECRETS_FILE, scopes=SCOPES)
+
+        # The URI created here must exactly match one of the authorized redirect URIs
+        # for the OAuth 2.0 client, which you configured in the API Console. If this
+        # value doesn't match an authorized URI, you will get a 'redirect_uri_mismatch'
+        # error.
+        flow.redirect_uri = ruri
+
+        authorization_url, state = flow.authorization_url(
+            # Enable offline access so that you can refresh an access token without
+            # re-prompting the user for permission. Recommended for web server apps.
+            access_type='offline',
+            # Enable incremental authorization. Recommended as a best practice.
+            include_granted_scopes='true')
+        return (authorization_url, state)
     
     def generateAPIkey(self):
         try:
