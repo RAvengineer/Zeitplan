4be972c9c2b07d4c9183268e992f5eca29d723c4
Rahul Bera <bera2591@gmail.com>
2020-04-01 11:34:41 +0530

    CalendarAPI.py: Remove redundant generateAPIkey()

--- Utilities/CalendarAPI.py
+++ Utilities/CalendarAPI.py
@@ -53,16 +53,6 @@ class googleCalendarAPI():
         except Exception as e:
             raise Exception(f"Error in CalendarAPI.py: getCreds(): {str(e)}")
     
-    def generateAPIkey(self):
-        try:
-            SCOPES = ['https://www.googleapis.com/auth/calendar.events']
-            # SCOPES = ['https://www.googleapis.com/auth/calendar']
-            flow = InstalledAppFlow.from_client_secrets_file('Utilities/credentials.json', SCOPES)
-            creds = flow.run_local_server(port=0)
-            return creds
-        except Exception as e:
-            raise(Exception(f'Error in generateAPIkey of googleCalendarAPI creds creation:\n{str(e)}'))
-    
     def encodeCredentials(self, creds):
         try:
             return creds.to_json().encode()
