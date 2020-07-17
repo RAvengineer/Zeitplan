2e6b699098b9214c3b04454dcac29e25a1ad2bb6
Rahul Bera <bera2591@gmail.com>
2020-07-17 18:36:02 +0530

    Add Delete Cookies feature

---
+++ Zeitplan/static/js/base.js
@@ -0,0 +1,20 @@
+function main() {
+    // Clear cookies
+    $('#clearCookiesButton').on("click", function(){
+        console.log("Clearing cookies...");
+        var allCookies = document.cookie.split(';'); 
+                
+        // The "expire" attribute of every cookie is
+        // Set to "Thu, 01 Jan 1970 00:00:00 GMT"
+        for (var i = 0; i < allCookies.length; i++) 
+            document.cookie = allCookies[i] + "=;expires=" + new Date(0).toUTCString(); 
+        console.log("Cookies cleared!");
+    });
+}
+
+main();
+
+/*
+References:
+    https://www.geeksforgeeks.org/how-to-clear-all-cookies-using-javascript/
+*/
